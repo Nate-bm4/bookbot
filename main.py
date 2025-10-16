36ce9762b8c46d335daeae535970685c81be28bf
Nate-bm4 <nathanield14@gmail.com>
2025-10-15 21:23:01 -0700

    Final build

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_text_words, get_character_counts
+from stats import count_text_words, get_character_counts, sort_dict, sort_key
 
 
 def get_book_text(filepath):
@@ -8,12 +8,31 @@ def get_book_text(filepath):
 
 
 def main():
+    print("============ BOOKBOT ============\n")
+
     filepath = (
         "/Users/ndowns/Code/workspace/github/projects/bookbot/books/frankenstein.txt"
     )
+    # start grabbing the book
     book = get_book_text(filepath)
+
+    print(f"Analyzing book found at {filepath}\n")
+    print("----------- Word Count ----------\n")
+    # Calculate the amount of words in the book
     print(count_text_words(book))
-    print(get_character_counts(book))
+
+    char_counts = get_character_counts(book)
+    # sort the dictionary
+    sorted_dict = sort_dict(char_counts)
+
+    sorted_dict.sort(reverse=True, key=sort_key)
+    print(""" --------- Character Count -------\n""")
+    # Displaying character counts
+    for dict in sorted_dict:
+        char = dict["char"]
+        num = dict["num"]
+        if char.isalpha():
+            print(f"{char}: {num}")
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,25 @@
+# Add a new function to your stats.py file that takes the dictionary of characters and their counts and returns a sorted list of dictionaries.
+# Each dictionary should have two key-value pairs: one for the character itself and one for that character's count (e.g. {"char": "b", "num": 4868}).
+# Use the .sort() method:
+# Use a helper function to return the "num" key of each dictionary for comparison.
+# Sort the list from greatest to least by the count.
+# Import and call the function in main.py, and capture the result.
+# Print the report to the console as shown above. If the character is not an alphabetical character (using the .isalpha() method), just skip it. (Be sure to match the expected output in the CLI tests!)
+def sort_key(e):
+    return e["num"]
+
+
+def sort_dict(dict):
+    list_of_dicts = []
+    for entry in dict:
+        pretty_dict = {}
+        pretty_dict["char"] = entry
+        pretty_dict["num"] = dict[entry]
+        list_of_dicts.append(pretty_dict)
+
+    return list_of_dicts
+
+
 def get_character_counts(text):
     char_count = {}
     for word in text:
@@ -14,4 +36,4 @@ def count_text_words(text):
     num = text.split()
     count = len(num)
     # print(num)
-    return f"Found {count} total words"
+    return f"Found {count} total words\n"
